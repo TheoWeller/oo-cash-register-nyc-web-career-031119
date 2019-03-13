a12e74ad4a1923e284e1d6e1dc5ffa7f87352ef2
Theo Weller <weller.theodor@gmail.com>
2019-03-13 22:36:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   def self.apply_discount
       @discount = @total.to_f * 0.2
       @total = @total - @discount
-      "After the discount, the total comes to #{@total.to_nearest_i}."
+      "After the discount, the total comes to #{@total}."
     end
     # return "There is no discount to apply." if discount = nil
     # nil
